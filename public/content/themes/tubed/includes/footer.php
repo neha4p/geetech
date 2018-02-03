470f8aefc94bb6d843f8e91dcce88c5b463c50ba
Henry Fuentes <hfuentes@uxpublishing.com>
2018-02-03 00:38:43 -0600

    google analytics update

--- public/content/themes/tubed/includes/footer.php
+++ public/content/themes/tubed/includes/footer.php
@@ -145,14 +145,19 @@
 	</script>
 
 	<?php if(isset($settings->google_tracking_id)): ?>
-	  <script>
-	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	    ga('create', '<?= $settings->google_tracking_id ?>', 'auto');
-	    ga('send', 'pageview');
-	  </script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $settings->google_tracking_id;?>"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+        <?php  if(Auth::user()):?>
+            gtag('set', {'user_id': '<?=Auth::user()->id;?>'}); // Set the user ID using signed-in user_id.
+        <?php endif; ?>
+            gtag('config', '<?= $settings->google_tracking_id;?>');
+        </script>
+
 	<?php endif; ?>
 
 	<script><?= ThemeHelper::getThemeSetting(@$theme_settings->custom_js, '') ?></script>
