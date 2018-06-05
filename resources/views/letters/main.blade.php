f6e50eacc18b8cf759add185efb6b17c48019209
Henry Fuentes <hfuentes@uxpublishing.com>
2018-06-05 09:54:45 -0500

    remove fade

--- resources/views/letters/main.blade.php
+++ resources/views/letters/main.blade.php
@@ -613,7 +613,7 @@
 </footer>
 <!-- [E] Footer -->
 <!-- Start Signup Modal -->
-<div class="modal fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal" id="signup" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog modal-sm" role="document">
         <div class="modal-content">
             <div class="modal-body">
